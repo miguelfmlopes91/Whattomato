aaeac92d8467428c6e1f36ac8bc41bc677c6c2bf
miguelfmlopes91 <miguelpvz@hotmail.com>
2018-01-15 18:44:57 +0000

    update ui folder

--- app/src/main/java/com/example/miguel/whattomato/ui/MainActivity.java
+++ app/src/main/java/com/example/miguel/whattomato/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.miguel.whattomato;
+package com.example.miguel.whattomato.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.miguel.whattomato.R;
 
 public class MainActivity extends AppCompatActivity {
 
