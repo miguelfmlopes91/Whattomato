7882c664d6592910c4cdb91b5f5ae1a2d812fdf0
miguelfmlopes91 <miguelpvz@hotmail.com>
2018-02-15 22:09:34 +0000

    Navigation drawer and webview fragments with RxJava

---
+++ app/src/main/java/com/example/miguel/whattomato/ui/fragments/TimeTableFragment.java
@@ -0,0 +1,24 @@
+package com.example.miguel.whattomato.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.miguel.whattomato.R;
+
+
+public class TimeTableFragment extends Fragment {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.timetable, container, false);
+    }
+}
--- app/src/main/java/com/example/miguel/whattomato/ui/main/MainActivity.java
+++ app/src/main/java/com/example/miguel/whattomato/ui/main/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.miguel.whattomato.ui.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.miguel.whattomato.R;
+import com.example.miguel.whattomato.ui.fragments.AttendenceFragment;
+import com.example.miguel.whattomato.ui.fragments.ExamScheduleFragment;
+import com.example.miguel.whattomato.ui.fragments.ResultFragment;
+import com.example.miguel.whattomato.ui.fragments.TimeTableFragment;
+import com.example.miguel.whattomato.ui.fragments.WebViewFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -81,19 +89,44 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
+        Bundle bundle = new Bundle();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.home) {
+            fragment = new HomeFragment();
+        } else if (id == R.id.timeTable) {
+            fragment = new TimeTableFragment();
+        } else if (id == R.id.examSchedule) {
+            fragment = new ExamScheduleFragment();
+        } else if (id == R.id.attendence) {
+            fragment = new AttendenceFragment();
+        } else if (id == R.id.result) {
+            fragment = new ResultFragment();
+        } else if (id == R.id.fb) {
+            bundle.putString("url", "https://www.facebook.com/androidhungerAH");
+            fragment = new WebViewFragment();
+            fragment.setArguments(bundle);
+        } else if (id == R.id.gplus) {
+            bundle.putString("url", "https://plus.google.com/+Androidhunger");
+            fragment = new WebViewFragment();
+            fragment.setArguments(bundle);
+        } else if (id == R.id.twitter) {
+            bundle.putString("url", "https://www.twitter.com/android_hunger");
+            fragment = new WebViewFragment();
+            fragment.setArguments(bundle);
+        } else if (id == R.id.github) {
+            bundle.putString("url", "https://github.com/avinashn/androidhunger.com");
+            fragment = new WebViewFragment();
+            fragment.setArguments(bundle);
+        } else if (id == R.id.youtube) {
+            bundle.putString("url", "https://www.youtube.com/androidhunger");
+            fragment = new WebViewFragment();
+            fragment.setArguments(bundle);
+        }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
